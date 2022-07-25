7e262db1d129a6d5c463941cf2e59c1f6be65e4f
Tejas Mesta <tejasmesta@gmail.com>
2022-07-25 17:41:43 +0530

    Add files via upload

---
+++ word_search.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(board[i][j]==word.charAt(0) && dfs(board,i,j,0,word))
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean dfs(char[][] board,int i,int j,int count,String word)
+    {
+        if(count==word.length())
+        {
+            return true;
+        }
+        
+        if(i<0 || i>=board.length || j<0 || j>=board[i].length || board[i][j]!=word.charAt(count))
+        {
+            return false;
+        }
+        
+        char temp = board[i][j];
+        board[i][j] = ' ';
+        
+        boolean f = dfs(board,i+1,j,count+1,word) || dfs(board,i,j+1,count+1,word) || dfs(board,i-1,j,count+1,word) || dfs(board,i,j-1,count+1,word);
+        
+        board[i][j] = temp;
+        
+        return f;
+    }
+}
